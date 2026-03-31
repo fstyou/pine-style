93efe070934ba4dc3e9d63cfa206c42214935e48
枫上天游 <git@fstu.cc>
2026-03-31 10:04:03 +0800

    chore: 完善版权声明
    Signed-off-by: 枫上天游 <git@fstu.cc>

--- app/src/main/java/cc/fstu/pinestyle/data/PostureStatistics.kt
+++ app/src/main/java/cc/fstu/pinestyle/data/PostureStatistics.kt
@@ -1,3 +1,12 @@
+/*
+Pine Style
+Copyright (C) 2026 fstu
+This file is part of Pine Style.
+Pine Style is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Pine Style is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Pine Style. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package cc.fstu.pinestyle.data
 
 import java.util.*
